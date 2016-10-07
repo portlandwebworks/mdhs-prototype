c7874da7fcd4a8ccfc1ecc91e2571c9ca1fcff9b
rcharow <rachel.charow@portlandwebworks.com>
2016-10-07 11:21:32 -0400

    MDHS-17 - Update facility service to store current facilities after a search

--- src/main/resources/public/app/services/FacilityService.js
+++ src/main/resources/public/app/services/FacilityService.js
@@ -3,12 +3,20 @@ angular.module('mdhs').service('FacilityService', function ($http, $q) {
 
 	var service = this;
 
+  service.currentFacilities = [];
+
 	service.find = function (criteria) {
 		return $http.get('/api/facilities', {params: criteria}).then(function (resp) {
-			return $q.resolve(resp.data);
+      service.currentFacilities = resp.data;
+			return $q.resolve();
 		}, function (resp) {
+      service.currentFacilities = [];
 			return $q.reject(resp);
 		});
 	};
+
+  service.getCurrentFacilities = function(){
+    return service.currentFacilities;
+  }
 
 });
