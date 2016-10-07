e27df22bbf02d437cb25d4e467bc5b037b529bc1
rcharow <rachel.charow@portlandwebworks.com>
2016-10-07 10:52:03 -0400

    MDHS-17 - Add gender and age options to search options factory

--- src/main/resources/public/app/factories/SearchOptionsFactory.js
+++ src/main/resources/public/app/factories/SearchOptionsFactory.js
@@ -30,21 +30,42 @@ angular.module('mdhs').factory('SearchOptionsFactory', function searchOptionsFac
     //});
 
     var types = [
-      { id: 1, label: 'Test'},
-      { id: 2, label: 'Slot Contractor'},
-      { id: 3, label: 'Group Home'},
-      { id: 4, label: 'Center'},
-      { id: 5, label: 'Non-Relative In-Home'},
-      { id: 6, label: 'Relative In-Home'},
-      { id: 7, label: 'Non-Relative Out-of-Home'},
-      { id: 8, label: 'Relative Out-of-Home'},
+      { value: 1, label: 'Test'},
+      { value: 2, label: 'Slot Contractor'},
+      { value: 3, label: 'Group Home'},
+      { value: 4, label: 'Center'},
+      { value: 5, label: 'Non-Relative In-Home'},
+      { value: 6, label: 'Relative In-Home'},
+      { value: 7, label: 'Non-Relative Out-of-Home'},
+      { value: 8, label: 'Relative Out-of-Home'},
     ];
 
     return types;
   }
+
+  var getGenders = function(){
+    return [
+      { value: 3, label: 'Both'},
+      { value: 1, label: 'Boy'},
+      { value: 2, label: 'Girl'}
+    ]
+  }
+
+  var getAges = function(){
+    return [
+      { value: 1, label: '0-2 years'},
+      { value: 2, label: '3-6 years'},
+      { value: 3, label: '6-10 years'},
+      { value: 4, label: '11-14 years'},
+      { value: 5, label: '15-18 years'},
+    ]
+  }
+
   return {
-    getCounties:      getCounties,
-    getCities:        getCities,
-    getProviderTypes: getProviderTypes
+    getCounties:          getCounties,
+    getCities:            getCities,
+    getProviderTypes:     getProviderTypes,
+    getGenders:           getGenders,
+    getAges:              getAges
   };
 });
