325097ac81c503855aee889f1790d9efe8bbd6a1
rcharow <rachel.charow@portlandwebworks.com>
2016-10-07 11:26:54 -0400

    MDHS-17 - Remove placeholder code

--- src/main/resources/public/app/pages/search/index.js
+++ src/main/resources/public/app/pages/search/index.js
@@ -1,12 +1,6 @@
 angular.module('mdhs').component('search', {
   templateUrl: '/templates/app/pages/search/index',
-  controller: function (FacilityService) {
-    var ctrl = this;
-    ctrl.message = 'Hello MDHS!';
-    FacilityService.find().then(function (facilities) {
-      ctrl.facilities = facilities;
-    }, function(){
-      ctrl.facilities = [];
-    });
+  controller: function () {
+
   }
 });
