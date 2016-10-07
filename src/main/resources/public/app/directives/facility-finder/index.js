24243178851eba8acc9666710e1aa12cc3ea4f78
rcharow <rachel.charow@portlandwebworks.com>
2016-10-07 14:55:39 -0400

    MDHS-17 - Add functionality to add another child to the search

--- src/main/resources/public/app/directives/facility-finder/index.js
+++ src/main/resources/public/app/directives/facility-finder/index.js
@@ -12,25 +12,45 @@ angular.module('mdhs').directive('facilityFinder', function (SearchOptionsFactor
       $scope.genders        = SearchOptionsFactory.getGenders();
       $scope.ages           = SearchOptionsFactory.getAges();
 
-      $scope.test = [];
+     initializeFilters();
 
-      $scope.facilityFilters = {
-        distance: '10'
-      }
+      $scope.childInfo = [
+        { gender: null, age: null }
+      ];
 
       $scope.search = function(){
+        _.forEach($scope.childInfo, function(child){
+          if(!_.includes($scope.facilityFilters.genders,child.gender)){
+            $scope.facilityFilters.genders.push(child.gender);
+          }
+          if(!_.includes($scope.facilityFilters.ages,child.age)){
+            $scope.facilityFilters.ages.push(child.age);
+          }
+        });
+
         FacilityService.find($scope.facilityFilters)
           .then(function(){
 
           },function(){
 
           })
-      }
+      };
 
       $scope.reset = function(){
+        initializeFilters();
+        $scope.childInfo = [{ gender: null, age: null }];
+      }
+
+      $scope.addChild = function(){
+        $scope.childInfo.push({ gender: null, age: null });
+      }
+
+      function initializeFilters(){
         $scope.facilityFilters = {
-          distance: '10'
-        }
+          distance: '10',
+          genders: [],
+          ages: []
+        };
       }
     }
   }});
