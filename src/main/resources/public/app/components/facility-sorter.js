512dcdcbb975686378d73f3cbfd6124b3c32b405
Joseph Descalzota <joseph.descalzota@portlandwebworks.com>
2016-10-09 17:12:08 -0400

    MDHS-11: Creating sorter component structure.

---
+++ src/main/resources/public/app/components/facility-sorter.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular.module('mdhs').component('facilitySorter', {
+  templateUrl: '/templates/app/components/facility-sorter',
+  controller: function(FacilityService){
+
+  }
+});
