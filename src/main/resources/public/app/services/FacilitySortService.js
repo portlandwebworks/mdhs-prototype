91fca66543dcb5e7c9f03af083c708b566735558
Joseph Descalzota <joseph.descalzota@portlandwebworks.com>
2016-10-10 10:08:45 -0400

    MDHS-11: Add sort service

---
+++ src/main/resources/public/app/services/FacilitySortService.js
@@ -0,0 +1,33 @@
+angular.module('mdhs').service('FacilitySortService', function ($http, $q) {
+  var service = this;
+
+  var sortOrders = {
+    LOCATION_NAME_ASCENDING: 'SORT_LOCATION_NAME_ASCENDING',
+    RATING_DESCENDING: 'SORT_RATING_DESCENDING',
+    SIZE_ASCENDING: 'SORT_SIZE_ASCENDING',
+    SIZE_DESCENDING: 'SORT_SIZE_DESCENDING'
+  };
+
+  var sortSettings = {
+    sortBy: sortOrders.LOCATION_NAME_ASCENDING
+  };
+
+  var filterSettings = {
+    hasSlots: true,
+    hasNoHistory: false,
+    hasLicense: false
+  };
+
+  service.getSortSettings = function(){
+    return sortSettings;
+  };
+
+  service.getFilterSettings = function(){
+    return filterSettings;
+  };
+
+  service.getSortOrders = function(){
+    return sortOrders;
+  };
+
+});
