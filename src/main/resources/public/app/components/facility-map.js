87a9478358950a9d7dfee30fa2f16331acfcc6c0
rcharow <rachel.charow@portlandwebworks.com>
2016-10-11 14:29:19 -0400

    MDHS-15 - Set up click events for the map

--- src/main/resources/public/app/components/facility-map.js
+++ src/main/resources/public/app/components/facility-map.js
@@ -2,7 +2,7 @@
 
 angular.module('mdhs').component('facilityMap', {
   templateUrl: '/templates/app/components/facility-map',
-  controller: function (FacilityService) {
+  controller: function (FacilityService, uiGmapIsReady) {
     var controller = this;
 
     //Initialize the map
@@ -14,10 +14,43 @@ angular.module('mdhs').component('facilityMap', {
       zoom: 7
     };
 
+    //Set the default zoom and center of the map
     controller.map = angular.copy(defaultMapState);
 
+    //Bind the facilities to the facilities service
     controller.facilities = FacilityService.getCurrentFacilities();
 
+    //Initialize a gmap variable to communicate with the google map instance
+    var gMap;
 
+    //Add
+    var zoomToFacilities = function (facilities) {
+      if (gMap && facilities && facilities.length > 0) {
+        var bounds = new google.maps.LatLngBounds();
+        _.each(facilities, function (facility) {
+          bounds.extend(new google.maps.LatLng({lat: facility.lat, lng: facility.lng}));
+        });
+        gMap.fitBounds(bounds);
+      }
+    };
+
+    //When the map is ready, zoom to the current facilities
+    uiGmapIsReady.promise(1).then(function (instances) {
+      gMap = instances[0].map;
+      zoomToFacilities(controller.facilities);
+    });
+
+
+    //Set up click events for the markers
+    controller.mapClick = function(marker, eventName, model) {
+      controller.selectedFacility = marker.key;
+      gMap.setZoom(16);
+      gMap.setCenter(marker.getPosition());
+    };
+
+    controller.map.events = {
+      click: controller.mapClick
+    };
+    
   }
 });
