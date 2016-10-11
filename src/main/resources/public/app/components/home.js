8f9c76a5f855b6d8604210ac6a1d984712f3bc8c
Joseph Descalzota <joseph.descalzota@portlandwebworks.com>
2016-10-11 10:43:50 -0400

    MDHS-13: Moving homepage blurb to homepage template. Makes sense for future use of filter.

--- src/main/resources/public/app/components/home.js
+++ src/main/resources/public/app/components/home.js
@@ -13,8 +13,6 @@ angular.module('mdhs').component('home', {
       '/bg04.jpg'
     ];
 
-    controller.greeting = 'Welcome';
-    controller.message = 'Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Proin eget tortor risus. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Cras ultricies ligula sed magna dictum porta. Pellentesque in ipsum id orci porta dapibus. Curabitur aliquet quam id dui posuere blandit. Quisque velit nisi, pretium ut lacinia in, elementum id enim.';
     controller.image = imagesPath + images[Math.floor(Math.random() * images.length)];
 
   }
